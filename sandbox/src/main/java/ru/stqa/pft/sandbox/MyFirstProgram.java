daa9a68850a8e574c2457ca5e191114d9e42113b
MaggeeJ <jmakotkina@mail.ru>
2023-08-04 17:28:32 +0300

    Добавлен новый класс Point и расчет расстояния между точками в MyFirstProgram

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,6 +11,11 @@ public class MyFirstProgram {
 
         Rectangle r = new Rectangle(4,6);
         System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(5.8, 11);
+        Point p2 = new Point(2.11,27.43);
+        System.out.println("Расстояние между точками с координатами (" + p1.x + "; " + p1.y + ") и " +
+                "(" + p2.x + "; " + p2.y + ") = " + Point.distance(p1, p2));
     }
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody +"!");
