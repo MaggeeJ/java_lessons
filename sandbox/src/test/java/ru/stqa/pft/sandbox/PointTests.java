549130c117f0f148787b48ddb0272df86b66da9a
MaggeeJ <jmakotkina@mail.ru>
2023-08-05 16:49:16 +0300

    Добавлен тест на проверку правильности вычисления расстояния между двумя точками

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance(){
+        Point p1 = new Point(2,3);
+        Point p2 = new Point(4,5);
+        Assert.assertEquals(Point.distance(p1,p2),2.8284271247461903);
+    }
+}
