f4edd4b1d80e0c1342fa83cc20f78828685e2c51
MaggeeJ <jmakotkina@mail.ru>
2023-08-06 15:04:46 +0300

    Создан общий класс для методов тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -2,19 +2,18 @@ package ru.stqa.pft.addressbook;
 
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTests {
-  private WebDriver wb;
+  private WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    wb = new FirefoxDriver();
-    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wb.get("http://localhost/addressbook/");
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
     login("admin", "secret");
   }
 
@@ -29,67 +28,67 @@ public class ContactCreationTests {
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    wb.quit();
+    wd.quit();
   }
 
   private void logout() {
-    wb.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   private void gotoHomePage() {
-    wb.findElement(By.linkText("home")).click();
+    wd.findElement(By.linkText("home")).click();
   }
 
   private void submitContactCreation() {
-    wb.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
   private void fillContactForm(ContactData contactdata) {
-    wb.findElement(By.name("firstname")).click();
-    wb.findElement(By.name("firstname")).clear();
-    wb.findElement(By.name("firstname")).sendKeys(contactdata.getFirstname());
-    wb.findElement(By.name("middlename")).click();
-    wb.findElement(By.name("middlename")).clear();
-    wb.findElement(By.name("middlename")).sendKeys(contactdata.getMiddlename());
-    wb.findElement(By.name("lastname")).click();
-    wb.findElement(By.name("lastname")).clear();
-    wb.findElement(By.name("lastname")).sendKeys(contactdata.getLastname());
-    wb.findElement(By.name("nickname")).click();
-    wb.findElement(By.name("nickname")).clear();
-    wb.findElement(By.name("nickname")).sendKeys(contactdata.getNickname());
-    wb.findElement(By.name("company")).click();
-    wb.findElement(By.name("company")).clear();
-    wb.findElement(By.name("company")).sendKeys(contactdata.getCompany());
-    wb.findElement(By.name("address")).click();
-    wb.findElement(By.name("address")).clear();
-    wb.findElement(By.name("address")).sendKeys(contactdata.getAddress());
-    wb.findElement(By.name("mobile")).click();
-    wb.findElement(By.name("mobile")).clear();
-    wb.findElement(By.name("mobile")).sendKeys(contactdata.getMobile());
-    wb.findElement(By.name("email")).click();
-    wb.findElement(By.name("email")).clear();
-    wb.findElement(By.name("email")).sendKeys(contactdata.getEmail());
-    wb.findElement(By.name("bday")).click();
-    new Select(wb.findElement(By.name("bday"))).selectByVisibleText(contactdata.getBday());
-    wb.findElement(By.xpath("//option[@value='" + contactdata.getBday() + "']")).click();
-    wb.findElement(By.name("bmonth")).click();
-    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText(contactdata.getBmonth());
-    wb.findElement(By.xpath("//option[@value='" + contactdata.getBmonth() + "']")).click();
-    wb.findElement(By.name("byear")).click();
-    wb.findElement(By.name("byear")).clear();
-    wb.findElement(By.name("byear")).sendKeys(contactdata.getByear());
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactdata.getFirstname());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(contactdata.getMiddlename());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactdata.getLastname());
+    wd.findElement(By.name("nickname")).click();
+    wd.findElement(By.name("nickname")).clear();
+    wd.findElement(By.name("nickname")).sendKeys(contactdata.getNickname());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactdata.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactdata.getAddress());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactdata.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactdata.getEmail());
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactdata.getBday());
+    wd.findElement(By.xpath("//option[@value='" + contactdata.getBday() + "']")).click();
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactdata.getBmonth());
+    wd.findElement(By.xpath("//option[@value='" + contactdata.getBmonth() + "']")).click();
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(contactdata.getByear());
   }
 
   private void initContactCreation() {
-    wb.findElement(By.linkText("add new")).click();
+    wd.findElement(By.linkText("add new")).click();
   }
 
   private void login(String username, String password) {
-    wb.findElement(By.name("user")).clear();
-    wb.findElement(By.name("user")).sendKeys(username);
-    wb.findElement(By.name("pass")).clear();
-    wb.findElement(By.name("pass")).sendKeys(password);
-    wb.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,20 +1,8 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTests {
-    private WebDriver wb;
-
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wb = new FirefoxDriver();
-        wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wb.get("http://localhost/addressbook/");
-        login("admin", "secret");
-    }
+public class GroupCreationTests extends TestBase{
 
     @Test
     public void testGroupCreation() throws Exception {
@@ -24,46 +12,6 @@ public class GroupCreationTests {
         submitGroupCreation();
         gotoGroupPage();
         logout();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wb.quit();
-    }
-
-    private void login(String username, String password) {
-        wb.findElement(By.name("user")).clear();
-        wb.findElement(By.name("user")).sendKeys(username);
-        wb.findElement(By.name("pass")).clear();
-        wb.findElement(By.name("pass")).sendKeys(password);
-        wb.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-    private void logout() {
-        wb.findElement(By.linkText("Logout")).click();
-    }
-
-    private void submitGroupCreation() {
-        wb.findElement(By.name("submit")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        wb.findElement(By.name("group_name")).click();
-        wb.findElement(By.name("group_name")).clear();
-        wb.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wb.findElement(By.name("group_header")).click();
-        wb.findElement(By.name("group_header")).clear();
-        wb.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wb.findElement(By.name("group_footer")).click();
-        wb.findElement(By.name("group_footer")).clear();
-        wb.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    private void initGroupCreation() {
-        wb.findElement(By.name("new")).click();
-    }
-
-    private void gotoGroupPage() {
-        wb.findElement(By.linkText("groups")).click();
     }
 
 }
