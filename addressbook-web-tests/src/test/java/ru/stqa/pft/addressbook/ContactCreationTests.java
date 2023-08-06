91c731692473d2daf16731c2e3d44231322d2f14
MaggeeJ <jmakotkina@mail.ru>
2023-08-06 12:36:23 +0300

    Выполнен рефакторинг теста "Добавление контакта"

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,100 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ContactCreationTests {
+  private WebDriver wb;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wb = new FirefoxDriver();
+    wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wb.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    initContactCreation();
+    fillContactForm(new ContactData("Anton", "Vasil", "Makarov", "Pups", "Groovy", "Kazahstan, Gandolyerov 98", "+79881112233", "makarov@gmail.com", "1", "January", "2014"));
+    submitContactCreation();
+    gotoHomePage();
+    logout();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wb.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private void logout() {
+    wb.findElement(By.linkText("Logout")).click();
+  }
+
+  private void gotoHomePage() {
+    wb.findElement(By.linkText("home")).click();
+  }
+
+  private void submitContactCreation() {
+    wb.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillContactForm(ContactData contactdata) {
+    wb.findElement(By.name("firstname")).click();
+    wb.findElement(By.name("firstname")).clear();
+    wb.findElement(By.name("firstname")).sendKeys(contactdata.getFirstname());
+    wb.findElement(By.name("middlename")).click();
+    wb.findElement(By.name("middlename")).clear();
+    wb.findElement(By.name("middlename")).sendKeys(contactdata.getMiddlename());
+    wb.findElement(By.name("lastname")).click();
+    wb.findElement(By.name("lastname")).clear();
+    wb.findElement(By.name("lastname")).sendKeys(contactdata.getLastname());
+    wb.findElement(By.name("nickname")).click();
+    wb.findElement(By.name("nickname")).clear();
+    wb.findElement(By.name("nickname")).sendKeys(contactdata.getNickname());
+    wb.findElement(By.name("company")).click();
+    wb.findElement(By.name("company")).clear();
+    wb.findElement(By.name("company")).sendKeys(contactdata.getCompany());
+    wb.findElement(By.name("address")).click();
+    wb.findElement(By.name("address")).clear();
+    wb.findElement(By.name("address")).sendKeys(contactdata.getAddress());
+    wb.findElement(By.name("mobile")).click();
+    wb.findElement(By.name("mobile")).clear();
+    wb.findElement(By.name("mobile")).sendKeys(contactdata.getMobile());
+    wb.findElement(By.name("email")).click();
+    wb.findElement(By.name("email")).clear();
+    wb.findElement(By.name("email")).sendKeys(contactdata.getEmail());
+    wb.findElement(By.name("bday")).click();
+    new Select(wb.findElement(By.name("bday"))).selectByVisibleText(contactdata.getBday());
+    wb.findElement(By.xpath("//option[@value='" + contactdata.getBday() + "']")).click();
+    wb.findElement(By.name("bmonth")).click();
+    new Select(wb.findElement(By.name("bmonth"))).selectByVisibleText(contactdata.getBmonth());
+    wb.findElement(By.xpath("//option[@value='" + contactdata.getBmonth() + "']")).click();
+    wb.findElement(By.name("byear")).click();
+    wb.findElement(By.name("byear")).clear();
+    wb.findElement(By.name("byear")).sendKeys(contactdata.getByear());
+  }
+
+  private void initContactCreation() {
+    wb.findElement(By.linkText("add new")).click();
+  }
+
+  private void login(String username, String password) {
+    wb.findElement(By.name("user")).clear();
+    wb.findElement(By.name("user")).sendKeys(username);
+    wb.findElement(By.name("pass")).clear();
+    wb.findElement(By.name("pass")).sendKeys(password);
+    wb.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+}
