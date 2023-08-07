f69509b7772e1f36f119676377a7a3cfce598bec
MaggeeJ <jmakotkina@mail.ru>
2023-08-07 21:43:24 +0300

    Обеспечена проверка выполнения и обеспечения предусловия для тостов Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupsHelper().isThereAGroup()) {
+      app.getGroupsHelper().createGroup (new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupsHelper().selectGroup();
     app.getGroupsHelper().deleteSelectedGroups();
     app.getGroupsHelper().returnToGroupPage();
